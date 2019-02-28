a7200b1e3cdbad737717a099cf7e1a04aca72438
marcelo <marevalos@tedepsa.com>
2019-02-27 23:43:29 -0300

    Configure firebase for auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { RegisterComponent } from './register/register.component';
 import { FormsModule} from '@angular/forms';
 
+// Authentications modules
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+export const firebaseConfig = {
+	apiKey: "AIzaSyCvK3DdmoTzotaBi2gFwev46S6UByyEYSI",
+	authDomain: "jahecha-b637b.firebaseapp.com",
+	databaseURL: "https://jahecha-b637b.firebaseio.com",
+	projectId: "jahecha-b637b",
+	storageBucket: "jahecha-b637b.appspot.com",
+	messagingSenderId: "467169590612"
+};
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +29,9 @@ import { FormsModule} from '@angular/forms';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireAuthModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Login {
+  email: String;
+  password: String;
+}
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
+  login: Login = {
+    email: "",
+    password: ""
+  }
+  
   constructor() { }
 
   ngOnInit() {
